32a13e0e36408cec0feba2094800ed9b959f7785
treForbes <kuntrevousforbes@gmail.com>
2024-04-05 16:39:54 -0400

    update

--- RugbyTeamPageFormat.php
+++ RugbyTeamPageFormat.php
@@ -1,12 +1,13 @@
 <?php 
 function pageHeader($title, $logo, $itemArray)
 {
+    echo "<div style=\"background-color: #3498db; padding: 20px;\">";
     echo "<img src=\"$logo\" alt=\"$logo\" width=\"150\" height=\"150\">";
     foreach($itemArray as $item)
     {
         $headerLink = $item . ".php";
-        echo "<a href=\"$headerLink\"><span class=\"m-5\"></span>$item</a>";
+        echo "<a href=\"$headerLink\" style=\"color: white; text-decoration: none; margin-left: 20px;\">$item</a>";
     }
-    echo "<hr>";
+    echo "</div>";
 }
 ?>
