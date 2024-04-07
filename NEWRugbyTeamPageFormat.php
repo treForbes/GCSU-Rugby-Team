e3fef1d2a3ae78155340e8c12409de08a8adbb9b
Petros1221 <164905187+Petros1221@users.noreply.github.com>
2024-04-07 14:47:01 -0400

    fixed conflicts

--- NEWRugbyTeamPageFormat.php
+++ NEWRugbyTeamPageFormat.php
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-<?php 
-function pageHeader($title, $logo, $itemArray)
-{
-    echo "<div style=\"background-color: #3498db; padding: 20px;\">";
-    echo "<img src=\"$logo\" alt=\"$logo\" width=\"150\" height=\"150\">";
-    foreach($itemArray as $item)
-    {
-        $headerLink = $item . ".php";
-        echo "<a href=\"$headerLink\" style=\"color: white; text-decoration: none; margin-left: 20px;\">$item</a>";
-    }
-    echo "</div>";
-}
-?>
-=======
+
 <?php 
 function pageHeader($title, $logo, $itemArray,$activepage)
 {
@@ -63,4 +49,4 @@ function pageHeader($title, $logo, $itemArray,$activepage)
     echo "</div></div></div></nav>";//closes out all of the markup
 }
 ?>
->>>>>>> waiting-for-review
+
