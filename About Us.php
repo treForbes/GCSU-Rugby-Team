4e7a9f7240619cf72e5a09ee8ed16c5d8ffef9dc
Dylan <166045814+dylanalmond1@users.noreply.github.com>
2024-04-30 01:22:38 -0400

    Updated the styles, overview, about us
    Finished all of overview please review to make sure, finished 90% of about us all it needs now is information that I need to get, updated styles for the pictures on both pages.

--- About Us.php
+++ About Us.php
@@ -1,46 +1,41 @@
-<?php
-session_start();
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        
-    </style>
-  </head>
-  <body>
-  <div style="height:auto; width:auto;">
-
-
-
-  
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+  <header>
     <?php
+    require_once 'NEWRugbyTeamPageFormat.php';
+    $arr=array("Home","Overview","Scheduler","About Us","Contact Us","Login","Sign Up");
+    $pageURI=  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];   //Determines which page the user is on
+    $pageArr= explode("/",$pageURI); //At this point, '$pageURI' looks like this: "documents/code/Home.php"
+    $testint=0;
+    $currentPage  = $pageArr[count($pageArr)-1]; //this just selects the file name. E.g. "Home.php"
+    pageHeader("Home","./images/GCSURugbyClub.png",$arr, $currentPage);
+    ?>
+  </header>
   
-    require_once 'RugbyTeamPageFormat.php';
-  if(isset($_SESSION['admin'])) {
-    // User is logged in as an admin
-    $arr = array("Home","About Us","Scheduler","Logout","ConTact US","OverView");
-} elseif(isset($_SESSION['player'])) {
-    // User is logged in as a player
-    $arr = array("Home","About Us","Scheduler","Logout","ConTact US","OverView");
-} else {
-    // User is not logged in
-    $arr = array("Home","About Us","Scheduler","Login", "SignUp","ConTact US","OverView");
-}
-  $pageURI=  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];   //Determines which page the use is on
-  $pageArr= explode("/",$pageURI); //At this point, '$pageURI' looks like this: "documents/code/Home.php"
-  $testint=0;
-  $currentPage  = $pageArr[count($pageArr)-1]; //this just selects the file name. E.g. "Home.php"
-  pageHeader("Home","./images/GCSURugbyClub.png",$arr, $currentPage);
-  ?>
-  
-   
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="photo-gallery">
+          <img id="current-photo" class="img-fluid">
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="overview-section">
+          <h1>About Us</h1>
+          <p>Add info here.</p>
+        </div>
+      </div>
+    </div>
+  </div>  
 
-  </div>   
+  <script src="functions.js"></script> 
 </body>
 </html>
--- Overview.php
+++ Overview.php
@@ -1,46 +1,50 @@
-<?php
-session_start();
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        
-    </style>
-  </head>
-  <body>
-  <div style="height:auto; width:auto;">
-
-
-
-  
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="styles.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+  <header>
     <?php
+    require_once 'NEWRugbyTeamPageFormat.php';
+    $arr=array("Home","Overview","Scheduler","About Us","Contact Us","Login","Sign Up");
+    $pageURI=  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];   //Determines which page the user is on
+    $pageArr= explode("/",$pageURI); //At this point, '$pageURI' looks like this: "documents/code/Home.php"
+    $testint=0;
+    $currentPage  = $pageArr[count($pageArr)-1]; //this just selects the file name. E.g. "Home.php"
+    pageHeader("Home","./images/GCSURugbyClub.png",$arr, $currentPage);
+    ?>
+  </header>
   
-    require_once 'RugbyTeamPageFormat.php';
-  if(isset($_SESSION['admin'])) {
-    // User is logged in as an admin
-    $arr = array("Home","About Us","Scheduler","Logout","ConTact US","OverView");
-} elseif(isset($_SESSION['player'])) {
-    // User is logged in as a player
-    $arr = array("Home","About Us","Scheduler","Logout","ConTact US","OverView");
-} else {
-    // User is not logged in
-    $arr = array("Home","About Us","Scheduler","Login", "SignUp","ConTact US","OverView");
-}
-  $pageURI=  $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];   //Determines which page the use is on
-  $pageArr= explode("/",$pageURI); //At this point, '$pageURI' looks like this: "documents/code/Home.php"
-  $testint=0;
-  $currentPage  = $pageArr[count($pageArr)-1]; //this just selects the file name. E.g. "Home.php"
-  pageHeader("Home","./images/GCSURugbyClub.png",$arr, $currentPage);
-  ?>
-  
-   
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="photo-gallery">
+          <img id="current-photo" class="img-fluid">
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="overview-section">
+          <h1>Overview</h1>
+          <p>Rugby is a dynamic team sport that originated in England in the 19th century.</p>
+          <h2>How to play:</h2>
+          <p>Two teams of around 15 players each compete. Forwards focus on ball possession, while backs emphazie speed and scoring. Points are scored through tries (5 points), conversions (2 points), pentalty kicks (3 points), and drop goals (3 points).</p>
+          <h2>Rules:</h2>
+          <ul class="bullet-points">
+            <li>Passing is allowed in any direction except forward.</li>
+            <li>Tackling is fundamental; players must release the ball when tackled.</li>
+            <li>Offside rules apply.</li>
+            <li>Kicking forward is permitted, but possession must be maintained.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>  
 
-  </div>   
+  <script src="functions.js"></script> 
 </body>
 </html>
